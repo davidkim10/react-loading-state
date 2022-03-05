ccb3d478fb55808b3568d1ca1454a716550ad0b1
David K <56006438+davidkim10@users.noreply.github.com>
2022-03-04 21:59:57 -0500

    Create LoadingState.tsx

---
+++ LoadingState.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { getClassNames } from "./utils";
+import "./LoadingState.scss";
+
+const config = {
+  animation: {
+    pulse: "loading-state--pulse",
+    wave: "loading-state--wave",
+  },
+  variant: {
+    text: "loading-state--text",
+    rect: "loading-state--rect",
+    circle: "loading-state--circle",
+  },
+};
+
+interface ILoadingStateProps {
+  animation?: "pulse" | "wave";
+  className?: string;
+  height?: string | number;
+  style?: React.CSSProperties;
+  variant?: "text" | "rect" | "circle";
+  width?: string | number;
+}
+
+const formatCSSValue = (value) =>
+  typeof value === "number" ? `${value}px` : value;
+
+const LoadingState: React.FC<ILoadingStateProps> = ({
+  animation = "pulse",
+  className,
+  height = ".75rem",
+  style,
+  variant = "text",
+  width,
+}) => {
+  const styles = {
+    height: formatCSSValue(height),
+    width: formatCSSValue(width),
+    ...style,
+  };
+
+  const classNames = getClassNames([
+    "loading-state",
+    config.variant[variant],
+    config.animation[animation],
+    className,
+  ]);
+
+  return <div className={classNames} style={styles}></div>;
+};
+
+export default LoadingState;

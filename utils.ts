a94a098a10782017c41adb005e089748273ff9b8
David K <56006438+davidkim10@users.noreply.github.com>
2022-03-04 22:00:53 -0500

    Create utils.ts

---
+++ utils.ts
@@ -0,0 +1,24 @@
+type ClassNameArray = (string | boolean)[];
+
+type ClassNamesObj = {
+  [key: string]: ClassNameArray;
+};
+
+export const getClassNames = (classNames: ClassNameArray): string => {
+  return classNames.filter(Boolean).join(" ");
+};
+
+export const createClassNames = <T extends ClassNamesObj>(obj: T) => {
+  type TypedResult = { [key in keyof T]: string };
+
+  const result = Object.keys(obj).reduce((acc, key: string) => {
+    const classNameArray = obj[key];
+    const filteredClassNames = getClassNames(classNameArray);
+    return {
+      ...acc,
+      [key]: filteredClassNames,
+    };
+  }, {} as TypedResult);
+
+  return result;
+};
